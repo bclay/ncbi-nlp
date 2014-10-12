24f75c1fff29d54b18d4a9c9af3fac95348e31fb
Brynn Claypoole <bclay@seas.upenn.edu>
2014-10-11 20:36:03 -0400

    parse out stop words

--- pytok.py
+++ pytok.py
@@ -1,7 +1,6 @@
 #imports
 from nltk.tokenize import RegexpTokenizer
-
-#initialize
+from nltk.corpus import stopwords
 
 #create an array of tokens
 def create_tokens(str):
@@ -10,5 +9,13 @@ def create_tokens(str):
 	lc_words = [w.lower() for w in words]
 	return lc_words
 
-print create_tokens("This is a sentence.")
-	
+def remove_words(arr):
+	clean_arr = []
+	stop = stopwords.words('english')
+	for w in arr:
+		if w not in stop:
+			clean_arr.append(w)
+	return clean_arr
+
+tok_arr = create_tokens("This is a sentence.")
+print remove_words(tok_arr)	
