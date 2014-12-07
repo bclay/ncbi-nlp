28fb5a402928d04ed75cb5408553481997c1d3ca
Brynn Claypoole <bclay@seas.upenn.edu>
2014-12-06 23:18:32 -0500

    can get abstract from pubmed

--- ncbi_get.py
+++ ncbi_get.py
@@ -6,37 +6,43 @@ from Bio import Medline
 def get_request():
 	#get a list of pubmed ids
 	db = 'pubmed'
-	query = 'asthma[mesh]+AND+leukotrienes[mesh]+AND+2009[pdat]'
+	#query = 'asthma[mesh]+AND+leukotrienes[mesh]+AND+2009[pdat]'
 	base = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-	url = base+'esearch.fcgi?db='+db+'&term='+query+'&usehistory=y'
+	#url = base+'esearch.fcgi?db='+db+'&term='+query+'&usehistory=y'
+	url = base + 'esearch.fcgi?LinkName=gene_pubmed&from_uid=1056'
 	r = requests.get(url)
-	#print "Search"
-	#print r.text
+	print "Search"
+	print r.text
 	#get abstracts from the pubmed ids
+	#print 'doing something'
+	#pid = '20113659'
+	#url = base+'efetch.fcgi?db='+db+'&id='+pid+'&rettype=xml'
+	#r2 = requests.get(url)
+	#print 'through request'
+	#print r2.text
+	#lines = r2.text.split("\n")
+	#for line in lines:
+	#	parts = line.split(" ")
+	#	print parts[0]
+		#parts = line.split()
+		#print parts[0]
+	#record = Medline.parse(r2.text)
 
-	pid = '20113659'
-	url = base+'efetch.fcgi?db='+db+'&id='+pid+'&rettype=medline'
-	r2 = requests.get(url)
-	print r2.text
-	record = Medline.read(r2.text)
 
-
-	print record
+	#print record
 	#r2json = json.loads(r2.text)
 	#print r2json
 
 
 
 
-
+# useful pubmed parsing code
 
 	#root2 = ET.fromstring(r2.text)
-	#for abst in root2.iter('abstract'):
-	#	for sec in abst.iter('sec'):
-	#		for child in sec:
-				#print child.tag
-	#			if child.tag == 'p':
-	#				print child.text
+	#for abst in root2.iter('Abstract'):
+	#	for sec in abst.iter('AbstractText'):
+	#		print sec.text
+
 
 	#iterate through ids and return abstracts
 	#root = ET.fromstring(r.text)
